360a6ceea4b80226bc0ead53d0d30dcf18c575f7
mamorley88 <mamorley88@gmail.com>
2017-10-16 22:20:53 -0600

    fixed it!!!

--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +14,107 @@ namespace HelloWorld
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //public class TINT tint;
+
+
+
+
         public MainPage()
         {
             this.InitializeComponent();
+            //tint = new List<winTint>()
+            //{
+            //    TINT.winTint.blue,
+
+            //};
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(txtResults.Text);
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+
+
+
+            //validate text width is numeric
+
+
+            //validate height is numberic
+
+            CalculateTotals();
+
+
+
+
+        }
+        //private void TxtWidth_TextChanging(object sender, System.EventArgs e)
+        //
+        //}
+
+        private void CalculateTotals()
+        {
+            //calculate 
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+
+            widthString = TxtWidth.Text;
+            width = double.Parse(widthString);
+
+
+            heightString = txtHeight.Text;
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width + height);
+
+
+        
+
+            txtResults.Text = "The length of the wood is " + woodLength + " feet \r\n" +
+            "The area of the glass is " + glassArea + "square meters \r\n" +
+            "you have chosen the color " + ((ComboBoxItem)winTint.SelectedValue).Content + " \r\n " +
+            "we will ship you " + slider.Value.ToString() + " windows \r\n" +
+            DateTime.Now + " Is your order date";
+        }
+
+        private void TxtWidth_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+
+
+            int parsedValue;
+            if (!int.TryParse(TxtWidth.Text, out parsedValue))
+            {
+                TxtWidth.Background = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                TxtWidth.Background = new SolidColorBrush(Colors.White);
+
+
+            }
+            return;
+        }
+
+        private void txtHeight_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            int parsedValue;
+            if (!int.TryParse(txtHeight.Text, out parsedValue))
+            {
+                txtHeight.Background = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                txtHeight.Background = new SolidColorBrush(Colors.White);
+
+
+            }
+            return;
         }
     }
+    
+    
 }
